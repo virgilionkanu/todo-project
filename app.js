8417c02fab78ed7560bb22da5dc660cf4760d363
Virgilio Cano <virgiliocano53@gmail.com>
2020-06-24 17:34:08 -0300

    Add and remove todo done.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector(".todo-list");
 
 //Event Listeners
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 //Functions
 function addTodo(event) {
@@ -32,4 +33,23 @@ function addTodo(event) {
   todoList.appendChild(todoDiv);
   //Clear Todo Input Value
   todoInput.value = "";
+}
+
+function deleteCheck(event) {
+  const item = event.target;
+  //DELETE TODO
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.classList.add("fall");
+    //Animation
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+
+  //CHECH MARK
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
 }
